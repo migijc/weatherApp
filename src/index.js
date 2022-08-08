c6fe05994cfe5dafd75dcf97458b6f323693adf8
Miguel <migijc@gmail.com>
2022-08-08 02:45:16 -0400

    fixed bug causing degree units to display wrong temp

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import css from "./main.css"
 import getLocationData from "./weatherData"
 import weeklyWeathercss from "./weeklyWeather.css"
 getLocationData("Miami")
+let degreeUnitToggle=document.getElementById("degreeUnit")
+
 
 
 let locationInput=document.querySelector("input")
 let searchButton=document.querySelector("button")
-searchButton.onclick=()=>{getLocationData(locationInput.value)}
+searchButton.onclick=()=>{
+    getLocationData(locationInput.value)
+    degreeUnitToggle.classList="faranheit"
+    degreeUnitToggle.checked=false
+}
 
